ef470edd8b6ff3b414b94ace8b3ad397f93ecc1d
Dansel <daniel.sheppard91@gmail.com>
2017-01-06 17:59:08 +0100

    Added first part of IdentiyServer. Should now be able to show openid-configuration

---
+++ Project_2.1/IdentityServer/Clients.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections.Generic;
+using IdentityServer3.Core;
+using IdentityServer3.Core.Models;
+
+namespace Project_2._1.IdentityServer
+{
+    public static class Clients
+    {
+        public static IEnumerable<Client> Get()
+        {
+            return new List<Client> {
+            new Client {
+                //The ClientId is the unique ID of the client. This will be referenced by any future client applications in order to interact with the Identity Server.
+                ClientId = "implicitclient",
+                //ClientName will be the identifier displayed on any consent screens.
+                ClientName = "Example Implicit Client",
+                //Enabled allows you to enable and disable individual clients.
+                Enabled = true,
+                //Flow is an enum that sets the OpenID Connect flow for the client.Available flows are: Implicit, AuthorizationCode, Hybrid, ClientCredentials, ResourceOwner and Custom (default is Implicit).
+                Flow = Flows.Implicit,
+                //RequireConsent and AllowRememberConsent activate and control the consent screen for logins.These both default to true
+                RequireConsent = true,
+                //RedirectUris is a collection of URIs that tokens and authorisation codes can be returned to for this client.If a URI is not on this list, then it cannot interact with Identity Server using this client.
+                AllowRememberConsent = true,
+                //PostLogoutRedirectUris is a collection of URIs that Identity Server can redirect to upon logout. Otherwise the user will stay on the default logout success screen within the Identity Server.
+                RedirectUris =
+                  new List<string> {"https://localhost:44304/account/signInCallback"},
+                //ScopeRestrictions is a list of OpenID Connect scopes allowed to be requested and returned to this client.We can take advantage of the predefined enums provided by Identity Server here.Not setting this allows all scopes to be retrieved (defaults to an empty list).
+                PostLogoutRedirectUris =
+                  new List<string> {"https://localhost:44304/"},
+                AllowedScopes = new List<string> {
+                    Constants.StandardScopes.OpenId,
+                    Constants.StandardScopes.Profile,
+                    Constants.StandardScopes.Email
+                },
+                //AccessTokenType defaults to JWT. The other option for this is Reference, which stores the JWT locally and instead sends the client a reference to it, allowing you to easily revoke the token.
+                AccessTokenType = AccessTokenType.Jwt
+            }
+        };
+        }
+    }
+}
---
+++ Project_2.1/Startup.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Security.Cryptography.X509Certificates;
+using System.Web;
+using IdentityServer3.Core.Configuration;
+using Owin;
+using Project_2._1.IdentityServer;
+
+namespace Project_2._1
+{
+    public sealed class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            app.Map(
+                "/core",
+                coreApp => {
+                    coreApp.UseIdentityServer(new IdentityServerOptions
+                    {
+                        SiteName = "Standalone Identity Server",
+                        SigningCertificate = LoadCertificate(),
+                        Factory = new IdentityServerServiceFactory()
+                                .UseInMemoryClients(Clients.Get())
+                                .UseInMemoryScopes(Scopes.Get())
+                                .UseInMemoryUsers(Users.Get()),
+                        RequireSsl = true
+                    });
+                });
+        }
+
+        X509Certificate2 LoadCertificate()
+        {
+            return new X509Certificate2(
+                string.Format(@"{0}\bin\identityServer\cert.pfx", AppDomain.CurrentDomain.BaseDirectory), "idsrv3test");
+        }
+    }
+}
